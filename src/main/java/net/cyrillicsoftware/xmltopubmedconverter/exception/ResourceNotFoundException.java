4c3e4b707ebb71006d16bb87c4bba2060aa6a07b
Halil Kajevic <hkajevic1999@gmail.com>
2022-03-18 23:34:12 +0100

    Added specific exception handler

---
+++ src/main/java/net/cyrillicsoftware/xmltopubmedconverter/exception/ResourceNotFoundException.java
@@ -0,0 +1,42 @@
+package net.cyrillicsoftware.xmltopubmedconverter.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    private String resourceName;
+    private String fieldName;
+    private long fieldValue;
+
+    public ResourceNotFoundException(String message, String resourceName, String fieldName, long fieldValue) {
+        super(resourceName + " not found with " + fieldName + " : " + fieldValue);
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public long getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(long fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+}
